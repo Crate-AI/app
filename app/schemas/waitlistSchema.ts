3646196f786eb7085c7742a4ef1cbb77135b2732
Ahmed felfel <felfelahmeed@gmail.com>
2024-07-21 17:24:40 -0400

    add types

--- app/schemas/waitlistSchema.ts
+++ app/schemas/waitlistSchema.ts
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 export const waitlistSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
-  user_type: z.enum(["DJ", "Vinyl Collector", "Record Store"]),
+  user_type: z.enum(["DJ", "Record Store", "Record Collector", "Other"]),
 });
