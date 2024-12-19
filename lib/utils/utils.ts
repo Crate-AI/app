f5f14fa1dd57c14988662e01dbf2efb7a4d75606
Ahmed felfel <felfelahmeed@gmail.com>
2024-12-18 20:10:11 -0500

    chore: update project configuration and utilities

--- lib/utils/utils.ts
+++ lib/utils/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import type { ReleaseDetails, ReleaseTrack } from '@/types/discogs';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
