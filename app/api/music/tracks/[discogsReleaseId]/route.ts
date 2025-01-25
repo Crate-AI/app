d5477fe467c9ee9a71e8a63ca8518fe2302862b7
gov <info@govindmohan.com>
2025-01-25 15:05:02 -0500

    fix: use track analysis bpm instead of fake track bpm

--- app/api/music/tracks/[discogsReleaseId]/route.ts
+++ app/api/music/tracks/[discogsReleaseId]/route.ts
@@ -1,11 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { cookies, headers } from 'next/headers';
 import { createClient } from '@/lib/database/server';
-import { findTrackVideo } from '@/lib/api-clients/youtube/service';
-import { getDiscogsRelease } from '@/lib/api-clients/discogs';
-import { InsertCrateTrack } from '@/types';
-import type { Track, Artist } from '@/types/external-services/discogs';
-
+import { CollectionUtils } from '@/lib/database/serverUtils/collection';
 
 export async function GET(
   request: NextRequest,
@@ -16,8 +12,6 @@ export async function GET(
   },
 ) {
   try {
-    const headersList = headers();
-    const ip = headersList.get('x-forwarded-for') || 'unknown';
     const accessToken = cookies().get('access_token')?.value;
     const accessTokenSecret = cookies().get('access_token_secret')?.value;
 
@@ -29,104 +23,11 @@ export async function GET(
     }
 
     const supabase = await createClient();
-    const { data: tracks, error: trackError } = await supabase
-      .from('tracks')
-      .select('*')
-      .eq('discogs_release_id', params.discogsReleaseId);
 
-    if (trackError) {
-      throw trackError;
-    }
+    const { getReleaseTracks } = CollectionUtils(supabase);
+    const tracks = await getReleaseTracks(params.discogsReleaseId);
 
-    // If track is not in the database, fetch it from Discogs/YouTube
-    if (tracks.length === 0) {
-      const { release, remaining, reset, isLimited } = await getDiscogsRelease(
-        accessToken,
-        accessTokenSecret,
-        ip,
-        params.discogsReleaseId,
-      );
-
-      if (isLimited) {
-        return NextResponse.json(
-          { error: 'Rate limit exceeded' },
-          {
-            status: 429,
-            headers: {
-              'X-RateLimit-Limit': '60',
-              'X-RateLimit-Remaining': '0',
-              'X-RateLimit-Reset': reset.toString(),
-            },
-          },
-        );
-      }
-
-      const tracksWithMetadata = await Promise.all(
-        release.tracklist.map(async (track: Track) => {
-          try {
-            const videoId = await findTrackVideo(track, release);
-            return {
-              ...track,
-              bpm: Math.floor(Math.random() * (140 - 115) + 115),
-              videoId,
-            };
-          } catch {
-            return {
-              ...track,
-              bpm: Math.floor(Math.random() * (140 - 115) + 115),
-              videoId: null,
-            };
-          }
-        }),
-      );
-
-      // Insert tracks and release into the database
-      const { error: releaseError } = await supabase
-        .from('discogs_releases')
-        .upsert({
-          discogs_release_id: params.discogsReleaseId,
-          discogs_release_data: release as any,
-        });
-
-      if (releaseError) {
-        console.error('Could not insert release', releaseError);
-        throw releaseError;
-      }
-
-      const fetchedTracks: InsertCrateTrack[] = tracksWithMetadata.map(
-        (track: Track & { bpm: number; videoId: string | null }) => ({
-          discogs_release_id: params.discogsReleaseId,
-          bpm: track.bpm,
-          youtube_video_id: track.videoId,
-          title: track.title,
-          position: track.position,
-          extra_artists: track.extraartists
-            ?.map((artist: Artist) => artist.name)
-            .join(', '),
-          artist: release.artists.map((artist: Artist) => artist.name).join(', '),
-          duration: track.duration,
-        }),
-      );
-
-      const { data: newTrackData, error: newTrackError } = await supabase
-        .from('tracks')
-        .upsert(fetchedTracks)
-        .select();
-
-      if (newTrackError) {
-        throw newTrackError;
-      }
-
-      return NextResponse.json(newTrackData, {
-        headers: {
-          'X-RateLimit-Limit': '60',
-          'X-RateLimit-Remaining': remaining.toString(),
-          'X-RateLimit-Reset': reset.toString(),
-        },
-      });
-    }
-
-    return NextResponse.json(tracks);
+    return NextResponse.json(tracks, { status: 200 });
   } catch (error) {
     console.error('Could not fetch release', error);
     return NextResponse.json(
@@ -134,8 +35,4 @@ export async function GET(
       { status: 500 },
     );
   }
-}
-
-async function processTrack(track: Track, artists: Artist[]) {
-  // ... existing code ...
 }
