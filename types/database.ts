db4ff05d1afea969267652b405fbb64235a9db82
gov <info@govindmohan.com>
2025-02-04 17:32:33 -0500

    fix: add function to get user collection tracks with JOIN

--- types/database.ts
+++ types/database.ts
@@ -2,6 +2,8 @@ import { Database } from './database/supabase';
 
 // Track types
 export type InsertCrateTrack = Database['public']['Tables']['tracks']['Insert'];
+// we want the CrateTrack type to not care about implemeetation details
+// for convenience, i.e. that track analysis info is stacked on separately
 export type CrateTrack = Database['public']['Tables']['tracks']['Row'] & {
   bpm: number | null;
 };
