dba86da278d89196a4ab9a56aef5b3bd89321c97
Ahmed felfel <36571501+paprikaf@users.noreply.github.com>
2025-02-10 14:03:25 -0500

    deploy biatch

--- app/page.tsx
+++ app/page.tsx
@@ -79,7 +79,6 @@ export const metadata: Metadata = {
 // };
 
 const Home: NextPage = async () => {
-  console.log('Home page');
   // const { username, resource_url } = StorageService.getItem('userIdentity');
   // const userDetails = await fetchUserDetails(resource_url);
   // const userCollection = await fetchUserCollection(username);
